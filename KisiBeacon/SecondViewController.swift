d963b247717b9726900aa511e313a176b753f1c6
Suyash Taneja <suyashb734@gmail.com>
2017-11-04 16:28:27 +0530

    Initial Commit

---
+++ KisiBeacon/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  KisiBeacon
+//
+//  Created by Suyash Taneja on 04/11/17.
+//  Copyright © 2017 Suyash Taneja. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
